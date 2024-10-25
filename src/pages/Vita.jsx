bc7ea5a803f580472bd9a4029412819eb0796eed
ihl-architektur <isabelle.ihl@gmx.de>
2024-10-25 12:24:17 +0200

    Vita next try

--- src/pages/Vita.jsx
+++ src/pages/Vita.jsx
@@ -5,7 +5,8 @@ const Vita = () => {
   return (
     <Container>
       <div>
-      <html lang="de">
+      <!DOCTYPE html>
+<html lang="de">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
