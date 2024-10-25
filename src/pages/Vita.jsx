fd0587c301719387a60aa8f6e5d91d4d6a50cf44
ihl-architektur <isabelle.ihl@gmx.de>
2024-10-25 12:34:32 +0200

    Next try Lebenslauf

--- src/pages/Vita.jsx
+++ src/pages/Vita.jsx
@@ -6,6 +6,230 @@ const Vita = () => {
     <Container>
       <div>
         LEBENSLAUF
+        <section>
+          <h2
+            style={{
+              color: '#555',
+              borderBottom: '2px solid #ccc',
+              paddingBottom: '5px',
+              fontSize: '1.2em',
+            }}
+          >
+            Ausbildung
+          </h2>
+          <div style={{ paddingLeft: '20px' }}>
+            <div>
+              <span
+                style={{
+                  fontWeight: 'bold',
+                  color: '#888',
+                  width: '150px',
+                  display: 'inline-block',
+                }}
+              >
+                10/2016 - 02/2018
+              </span>
+              Master of Science (m. Auszeichnung), TU Darmstadt
+            </div>
+            <div>
+              <span
+                style={{
+                  fontWeight: 'bold',
+                  color: '#888',
+                  width: '150px',
+                  display: 'inline-block',
+                }}
+              >
+                09/2015 - 06/2016
+              </span>
+              Austauschstudium, EPF Lausanne
+            </div>
+            <div>
+              <span
+                style={{
+                  fontWeight: 'bold',
+                  color: '#888',
+                  width: '150px',
+                  display: 'inline-block',
+                }}
+              >
+                10/2011 - 07/2014
+              </span>
+              Bachelor of Science, TU Darmstadt
+            </div>
+          </div>
+        </section>
+        <section>
+          <h2
+            style={{
+              color: '#555',
+              borderBottom: '2px solid #ccc',
+              paddingBottom: '5px',
+              fontSize: '1.2em',
+            }}
+          >
+            Beruflicher Werdegang
+          </h2>
+          <div style={{ paddingLeft: '20px' }}>
+            <div>
+              <span
+                style={{
+                  fontWeight: 'bold',
+                  color: '#888',
+                  width: '150px',
+                  display: 'inline-block',
+                }}
+              >
+                01/2024 -
+              </span>
+              Mitglied der Architektenkammer, Berlin
+            </div>
+            <div>
+              <span
+                style={{
+                  fontWeight: 'bold',
+                  color: '#888',
+                  width: '150px',
+                  display: 'inline-block',
+                }}
+              >
+                11/2023 -
+              </span>
+              Selbstständigkeit
+            </div>
+            <div>
+              <span
+                style={{
+                  fontWeight: 'bold',
+                  color: '#888',
+                  width: '150px',
+                  display: 'inline-block',
+                }}
+              >
+                07/2022 - 12/2023
+              </span>
+              Mitarbeiterin, Müller Reimann Architekten, Berlin
+            </div>
+            <div>
+              <span
+                style={{
+                  fontWeight: 'bold',
+                  color: '#888',
+                  width: '150px',
+                  display: 'inline-block',
+                }}
+              >
+                04/2019 - 04/2022
+              </span>
+              Mitarbeiterin, Heneghan peng architects, Berlin
+            </div>
+            <div>
+              <span
+                style={{
+                  fontWeight: 'bold',
+                  color: '#888',
+                  width: '150px',
+                  display: 'inline-block',
+                }}
+              >
+                04/2018 - 08/2018
+              </span>
+              Wiss. Mitarbeiterin, Entwerfen und Baukonstruktion, TU DA
+            </div>
+            <div>
+              <span
+                style={{
+                  fontWeight: 'bold',
+                  color: '#888',
+                  width: '150px',
+                  display: 'inline-block',
+                }}
+              >
+                06/2017 - 01/2018
+              </span>
+              Praktikantin, Waechter + Waechter Architekten, Darmstadt
+            </div>
+            <div>
+              <span
+                style={{
+                  fontWeight: 'bold',
+                  color: '#888',
+                  width: '150px',
+                  display: 'inline-block',
+                }}
+              >
+                06/2016 - 11/2016
+              </span>
+              Praktikantin, Dreier Frenzel Architekten, Lausanne
+            </div>
+            <div>
+              <span
+                style={{
+                  fontWeight: 'bold',
+                  color: '#888',
+                  width: '150px',
+                  display: 'inline-block',
+                }}
+              >
+                10/2014 - 08/2015
+              </span>
+              Tutorin, Entwerfen und Gebäudetechnologie, TU DA
+            </div>
+            <div>
+              <span
+                style={{
+                  fontWeight: 'bold',
+                  color: '#888',
+                  width: '150px',
+                  display: 'inline-block',
+                }}
+              >
+                10/2013 - 07/2014
+              </span>
+              Tutorin, Entwerfen und Stadtentwicklung, TU DA
+            </div>
+          </div>
+        </section>
+        <section>
+          <h2
+            style={{
+              color: '#555',
+              borderBottom: '2px solid #ccc',
+              paddingBottom: '5px',
+              fontSize: '1.2em',
+            }}
+          >
+            Nebenberufliche Engagements
+          </h2>
+          <div style={{ paddingLeft: '20px' }}>
+            <div>
+              <span
+                style={{
+                  fontWeight: 'bold',
+                  color: '#888',
+                  width: '150px',
+                  display: 'inline-block',
+                }}
+              >
+                04/2024
+              </span>
+              Architekturkollektiv 'LaPemit'
+            </div>
+            <div>
+              <span
+                style={{
+                  fontWeight: 'bold',
+                  color: '#888',
+                  width: '150px',
+                  display: 'inline-block',
+                }}
+              >
+                02/2020 - 03/2020
+              </span>
+              Soziales Projekt 'Thusanang', Südafrika
+            </div>
+          </div>
+        </section>
         <p>das ist ein P</p>
       </div>
     </Container>
