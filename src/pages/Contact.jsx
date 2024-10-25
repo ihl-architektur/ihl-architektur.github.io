5c04c88b59b27f34f4b9e6c0b9274c26cb3a4342
ihl-architektur <isabelle.ihl@gmx.de>
2024-10-25 16:40:06 +0200

    Kontakt Zweispaltig

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,26 @@ import Container from 'utils/Container';
 const Contact = () => {
   return (
     <Container>
-      <div>Kontakt</div>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'flex-start',
+          fontFamily: 'Arial, sans-serif',
+          color: '#333',
+        }}
+      >
+        <div style={{ marginRight: '2em' }}>
+          <p style={{ margin: 0 }}>KONTAKT</p>
+        </div>
+        <div>
+          <p style={{ margin: 0 }}>Isabelle Ihl</p>
+          <p style={{ margin: 0 }}>M.Sc. Architektin</p>
+          <p style={{ margin: 0 }}>Damaschkestraße 34</p>
+          <p style={{ margin: 0 }}>D-10711 Berlin</p>
+          <p style={{ margin: 0 }}>+49 15733927022</p>
+          <p style={{ margin: 0 }}>info@ihl-architektur.de</p>
+        </div>
+      </div>
     </Container>
   );
 };
