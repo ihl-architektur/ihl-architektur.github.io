c07c4b1f85f62def404514296bac358b17e50f81
ihl-architektur <isabelle.ihl@gmx.de>
2024-10-28 16:36:22 +0100

    Imprint - Test 1

--- src/pages/DataPrivacy.jsx
+++ src/pages/DataPrivacy.jsx
@@ -5,221 +5,47 @@ const DataPrivacy = () => {
   return (
     <Container>
       <div>
-        <h1>DATENSCHUTZ</h1>
 
-        <h2>1. DATENSCHUTZERKLÄRUNG</h2>
+<h1 style={{ margin: '2em 0' }}>Datenschutz</h1>
 
-        <h3>1.1 DATENSCHUTZ AUF EINEN BLICK</h3>
+<h2 style={{ margin: '2em 0' }}>1. DATENSCHUTZERKLÄRUNG</h2>
 
-        <p>
-          <strong>Allgemeine Hinweise</strong>
-          <br />
-          Die folgenden Hinweise geben einen einfachen Überblick darüber, was
-          mit Ihren personenbezogenen Daten passiert, wenn Sie unsere Website
-          besuchen. Personenbezogene Daten sind alle Daten, mit denen Sie
-          persönlich identifiziert werden können.
-        </p>
+<h3 style={{ margin: '2em 0' }}>1.1 DATENSCHUTZ AUF EINEN BLICK</h3>
 
-        <p>
-          <strong>Datenerfassung auf unserer Website</strong>
-          <br />
-          Das Büro erfasst während Ihrem Besuch der Website keine eigenen Daten.
-          Die Datenverarbeitung auf dieser Website erfolgt ausschließlich durch
-          den Websitebetreiber. Dessen Kontaktdaten können Sie dem Impressum
-          dieser Website entnehmen.
-        </p>
+<p><strong>Allgemeine Hinweise</strong><br />
+    Die folgenden Hinweise geben einen einfachen Überblick darüber, was mit Ihren personenbezogenen Daten passiert, wenn Sie unsere Website besuchen. Personenbezogene Daten sind alle Daten, mit denen Sie persönlich identifiziert werden können.
+</p>
 
-        <p>
-          <strong>Wie erfassen wir Ihre Daten?</strong>
-          <br />
-          Ihre Daten werden zum einen dadurch erhoben, dass Sie uns diese
-          mitteilen. Hierbei kann es sich z.B. um Daten handeln, die Sie uns bei
-          einem Kontakt per E-Mail mitteilen. Andere personenbezogene Daten
-          werden automatisch beim Besuch der Website durch IT-Systeme des
-          Websitebetreibers erfasst. Das sind vor allem technische Daten (z.B.
-          Internetbrowser, Betriebssystem oder Uhrzeit des Seitenaufrufs). Die
-          Erfassung dieser Daten erfolgt automatisch, sobald Sie unsere Website
-          betreten.
-        </p>
+<p><strong>Datenerfassung auf unserer Website</strong><br />
+    Das Büro erfasst während Ihrem Besuch der Website keine eigenen Daten. Die Datenverarbeitung auf dieser Website erfolgt ausschließlich durch den Websitebetreiber. Dessen Kontaktdaten können Sie dem Impressum dieser Website entnehmen.
+</p>
 
-        <p>
-          <strong>Wofür nutzen wir Ihre Daten?</strong>
-          <br />
-          Ein Teil der Daten wird erhoben, um eine fehlerfreie Bereitstellung
-          der Website zu gewährleisten. Andere Daten können zur Analyse Ihres
-          Nutzerverhaltens verwendet werden.
-        </p>
+<h3 style={{ margin: '2em 0' }}>1.2 HOSTING</h3>
 
-        <p>
-          <strong>Welche Rechte haben Sie bezüglich Ihrer Daten?</strong>
-          <br />
-          Sie haben jederzeit das Recht unentgeltlich Auskunft über Herkunft,
-          Empfänger und Zweck Ihrer gespeicherten, personenbezogenen Daten zu
-          erhalten. Sie haben außerdem ein Recht, die Berichtigung, Sperrung
-          oder Löschung dieser Daten zu verlangen. Hierzu sowie zu weiteren
-          Fragen zum Thema Datenschutz können Sie sich jederzeit unter der im
-          Impressum angegebenen Adresse an uns wenden. Des Weiteren steht Ihnen
-          gesetzlich ein Beschwerderecht bei der zuständigen Aufsichtsbehörde
-          zu.
-        </p>
+<p><strong>Externes Hosting</strong><br />
+    Ich nutze die Dienste der Namecheap Inc., 4600 East Washington Street, Suite 305, Phoenix, AZ 85034, USA zum Webhosting für meine Webseite und habe dazu einen Vertrag zur Auftragsverarbeitung gemäß Art. 28 DSGVO mit der Namescheap Inc. geschlossen. Weitere Informationen erhalten Sie in der Datenschutzerklärung der Namecheap Inc. unter <a href="https://www.namecheap.com/about/privacy-commitment/">https://www.namecheap.com/about/privacy-commitment/</a>. Rechtsgrundlage ist unser berechtigtes Interesse des Betriebes und des Erhalts der Betriebssicherheit dieser Webseite gemäß Art. 6 Abs. 1 S. 1 lit. f DSGVO.
+</p>
 
-        <p>
-          <strong>Analyse-Tools und Tools von Drittanbietern</strong>
-          <br />
-          Beim Besuch unserer Website kann Ihr Surf-Verhalten statistisch
-          ausgewertet werden. Das geschieht vor allem mit sogenannten
-          Analyseprogrammen. Die Analyse Ihres Surf-Verhaltens erfolgt in der
-          Regel anonym; das Surf-Verhalten kann nicht zu Ihnen zurückverfolgt
-          werden. Sie können dieser Analyse widersprechen. Über die
-          Widerspruchsmöglichkeiten werden wir Sie in dieser
-          Datenschutzerklärung informieren.
-        </p>
+<h3 style={{ margin: '2em 0' }}>1.3 ALLGEMEINE HINWEISE UND PFLICHTINFORMATIONEN</h3>
 
-        <h3>1.2 HOSTING</h3>
+<p><strong>Datenschutz</strong><br />
+    Die Betreiber dieser Seiten nehmen den Schutz Ihrer persönlichen Daten sehr ernst. Wir behandeln Ihre personenbezogenen Daten vertraulich und entsprechend der gesetzlichen Datenschutzvorschriften sowie dieser Datenschutzerklärung. Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.
+</p>
 
-        <p>
-          <strong>Externes Hosting</strong>
-          <br />
-          Ich nutze die Dienste der Namecheap Inc., 4600 East Washington Street,
-          Suite 305, Phoenix, AZ 85034, USA zum Webhosting für meine Webseite
-          und habe dazu einen Vertrag zur Auftragsverarbeitung gemäß Art. 28
-          DSGVO mit der Namescheap Inc. geschlossen. Weitere Informationen
-          erhalten Sie in der Datenschutzerklärung der Namecheap Inc. unter{' '}
-          <a href="https://www.namecheap.com/about/privacy-commitment/">
-            https://www.namecheap.com/about/privacy-commitment/
-          </a>
-          . Rechtsgrundlage ist unser berechtigtes Interesse des Betriebes und
-          des Erhalts der Betriebssicherheit dieser Webseite gemäß Art. 6 Abs. 1
-          S. 1 lit. f DSGVO.
-        </p>
+<h2 style={{ margin: '2em 0' }}>2. HAFTUNGSAUSSCHLUSS</h2>
 
-        <h3>1.3 ALLGEMEINES HINWEISE UND PFLICHTINFORMATIONEN</h3>
+<h3 style={{ margin: '2em 0' }}>2.1 HAFTUNG FÜR INHALTE</h3>
 
-        <p>
-          <strong>Datenschutz</strong>
-          <br />
-          Die Betreiber dieser Seiten nehmen den Schutz Ihrer persönlichen Daten
-          sehr ernst. Wir behandeln Ihre personenbezogenen Daten vertraulich und
-          entsprechend der gesetzlichen Datenschutzvorschriften sowie dieser
-          Datenschutzerklärung. Wir weisen darauf hin, dass die Datenübertragung
-          im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken
-          aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch
-          Dritte ist nicht möglich.
-        </p>
+<p>Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
+</p>
 
-        <p>
-          <strong>
-            Die verantwortliche Stelle für die Datenverarbeitung auf dieser
-            Website ist:
-          </strong>
-          <br />
-          Isabelle Ihl
-          <br />
-          Damaschkestraße 34
-          <br />
-          10711 Berlin
-          <br />
-          info@ihl-architektur.de
-        </p>
+<h3 style={{ margin: '2em 0' }}>2.2 HAFTUNG FÜR LINKS</h3>
 
-        <p>
-          <strong>Widerruf Ihrer Einwilligung zur Datenverarbeitung</strong>
-          <br />
-          Viele Datenverarbeitungsvorgänge sind nur mit Ihrer ausdrücklichen
-          Einwilligung möglich. Sie können eine bereits erteilte Einwilligung
-          jederzeit widerrufen. Dazu reicht eine formlose Mitteilung per E-Mail
-          an uns. Die Rechtmäßigkeit der bis zum Widerruf erfolgten
-          Datenverarbeitung bleibt vom Widerruf unberührt.
-        </p>
+<p>Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+</p>
 
-        <p>
-          <strong>Beschwerderecht bei der zuständigen Aufsichtsbehörde</strong>
-          <br />
-          Im Falle datenschutzrechtlicher Verstöße steht dem Betroffenen ein
-          Beschwerderecht bei der zuständigen Aufsichtsbehörde zu. Zuständige
-          Aufsichtsbehörde in datenschutzrechtlichen Fragen ist der
-          Landesdatenschutzbeauftragte des Bundeslandes, in dem unser
-          Unternehmen seinen Sitz hat. Eine Liste der Datenschutzbeauftragten
-          sowie deren Kontaktdaten können folgendem Link entnommen werden:{' '}
-          <a href="https://www.bfdi.bund.de/DE/Infothek/AnschriftenLinks/anschriftenlinks-node.html">
-            https://www.bfdi.bund.de/DE/Infothek/AnschriftenLinks/anschriftenlinks-node.html
-          </a>
-          .
-        </p>
-
-        <p>
-          <strong>Recht auf Datenübertragbarkeit</strong>
-          <br />
-          Sie haben das Recht, Daten, die wir auf Grundlage Ihrer Einwilligung
-          oder in Erfüllung eines Vertrags automatisiert verarbeiten, an sich
-          oder an einen Dritten in einem gängigen, maschinenlesbaren Format
-          aushändigen zu lassen. Sofern Sie die direkte Übertragung der Daten an
-          einen anderen Verantwortlichen verlangen, erfolgt dies nur, soweit es
-          technisch machbar ist.
-        </p>
-
-        <p>
-          <strong>SSL- bzw. TLS-Verschlüsselung</strong>
-          <br />
-          Diese Seite nutzt aus Sicherheitsgründen eine SSL- bzw.
-          TLS-Verschlüsselung. Eine verschlüsselte Verbindung erkennen Sie
-          daran, dass die Adresszeile des Browsers von “http://” auf “https://”
-          wechselt und an dem Schloss-Symbol in Ihrer Browserzeile. Wenn die
-          SSL- bzw. TLS-Verschlüsselung aktiviert ist, können die Daten, die Sie
-          an uns übermitteln, nicht von Dritten mitgelesen werden.
-        </p>
-
-        <p>
-          <strong>Auskunft, Sperrung, Löschung</strong>
-          <br />
-          Sie haben im Rahmen der geltenden gesetzlichen Bestimmungen jederzeit
-          das Recht auf unentgeltliche Auskunft über Ihre gespeicherten
-          personenbezogenen Daten, deren Herkunft und Empfänger und den Zweck
-          der Datenverarbeitung und ggf. ein Recht auf Berichtigung, Sperrung
-          oder Löschung dieser Daten. Hierzu sowie zu weiteren Fragen zum Thema
-          personenbezogene Daten können Sie sich jederzeit unter der im
-          Impressum angegebenen Adresse an uns wenden.
-        </p>
-
-        <h2>2. HAFTUNGSAUSSCHLUSS</h2>
-
-        <h3>2.1 HAFTUNG FÜR INHALTE</h3>
-
-        <p>
-          Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für
-          die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir
-          jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7
-          Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen
-          Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als
-          Diensteanbieter jedoch nicht verpflichtet, übermittelte oder
-          gespeicherte fremde Informationen zu überwachen oder nach Umständen zu
-          forschen, die auf eine rechtswidrige Tätigkeit hinweisen.
-          Verpflichtungen zur Entfernung oder Sperrung der Nutzung von
-          Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt.
-          Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der
-          Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden
-          von entsprechenden Rechtsverletzungen werden wir diese Inhalte
-          umgehend entfernen.
-        </p>
-
-        <h3>2.2 HAFTUNG FÜR LINKS</h3>
-
-        <p>
-          Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren
-          Inhalte wir keinen Einfluss haben. Deshalb können wir für diese
-          fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der
-          verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der
-          Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der
-          Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige
-          Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine
-          permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne
-          konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei
-          Bekanntwerden von Rechtsverletzungen werden wir derartige Links
-          umgehend entfernen.
-        </p>
-      </div>
+</div>
     </Container>
   );
-};
 
 export default DataPrivacy;
