0a00eb02f4bac05f75d6bfa2d692c12dcafb9425
kkroth <kaikroth@googlmail.com>
2024-12-05 18:12:01 +0100

    cleanup and optimizing hover effect

--- src/components/ImageSwiper/ImageSwiper.jsx
+++ src/components/ImageSwiper/ImageSwiper.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FreeMode } from 'swiper/modules';
 import './swiper.css';
 
 const ImageSwiper = ({ imageArray }) => {
-  const [hovered, setHovered] = useState(null);
   const navigate = useNavigate();
 
   const handleProjectClick = (link) => {
@@ -24,31 +22,22 @@ const ImageSwiper = ({ imageArray }) => {
       >
         {imageArray.map((image, index) => (
           <SwiperSlide key={index}>
-            <div
-              onMouseEnter={() => setHovered(index)}
-              onMouseLeave={() => setHovered(null)}
-              onClick={() => handleProjectClick(image.link)}
-              onTouchStart={() => setHovered(index)}
-              onTouchEnd={() => setHovered(null)}
-            >
+            <div onClick={() => handleProjectClick(image.link)}>
               <img
                 src={image.src}
                 alt={`Slide ${index + 1}`}
                 className="max-h-[300px] md:max-h-[400px]"
                 loading="lazy"
               />
-              {hovered === index && (
-                <div
-                  className={`px-4 absolute flex-col inset-0 bg-secondary bg-opacity-80 flex items-center justify-center text-black text-center text-lg font-semibold transition-opacity duration-300 ${
-                    hovered === index ? 'opacity-100' : 'opacity-0'
-                  }`}
-                >
-                  {image?.title?.toUpperCase()}
-                  <p className="mx-4 text-sm font-normal">
-                    {image?.subtitle?.toUpperCase()}
-                  </p>
-                </div>
-              )}
+
+              <div
+                className={`px-4 absolute flex-col inset-0 bg-secondary bg-opacity-80 opacity-0 flex items-center justify-center text-black text-center text-lg font-semibold transition-opacity duration-300 hover:opacity-100 hover:delay-300`}
+              >
+                {image?.title?.toUpperCase()}
+                <p className="mx-4 text-sm font-normal select-none">
+                  {image?.subtitle?.toUpperCase()}
+                </p>
+              </div>
             </div>
           </SwiperSlide>
         ))}
