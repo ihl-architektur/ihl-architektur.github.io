85d5ad2c53ba6505ee4bbfe9a6ab35b13fc53f8b
ihl-architektur <isabelle.ihl@gmx.de>
2026-01-24 11:56:26 +0100

    11 verändert

--- src/pages/Vita.jsx
+++ src/pages/Vita.jsx
@@ -160,7 +160,7 @@ const Vita = () => {
         </tr>
         <tr>
           <td className="font-light flex flex-row justify-between min-w-[140px]">
-            <div>1 1|2025 </div>
+            <div> 1 1|2025 </div>
           </td>
           <td className="pl-4">Vortrag - Architektinnengespräch, Berlin</td>
         </tr>
